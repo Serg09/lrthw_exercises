cdaedfbc09ed3f13e154c18cd5626967d6716646
Serg09 <sergeyskumatov@aol.com>
2015-10-02 12:22:12 -0600

    Add Readme.md file and ex3.rb, ex3_floating.rb, irb_calculator.md files
    [#104389658]

---
+++ chapter_3/ex3.rb
@@ -0,0 +1,33 @@
+# print a string
+puts 'I will now count my chickens:'
+
+# prints a combination of strings and code (used math arithmetic)
+puts 'Hens #{25+30/6}'
+puts 'Roosters #{100-25*3%4}'
+
+# print a string
+puts 'Now I will count the eggs:'
+
+# print a result of math arithmetic
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# print a string
+puts 'Is it true that 3+2<5-7?'
+
+# print a math arithmetic
+puts 3 + 2 < 5 - 7
+
+# prints a combination of strings and code (used math arithmetic)
+puts 'what is 3+2? #{3+2}'
+puts 'What is 5-7? #{5-7}'
+
+# print a string
+puts 'Oh, that\'s why it\'s false.'
+
+# print a string
+puts 'How about some more.'
+
+# prints a combination of strings and code (logic operator)
+puts 'Is it greater? #{5>-2}'
+puts 'is it greater or equal? #{5>=-2}'
+puts 'Is it less or equal? #{5<=-2}'
---
+++ chapter_3/ex3_floating.rb
@@ -0,0 +1,33 @@
+# print a string
+puts 'I will now count my chickens:'
+
+# prints a combination of strings and code (used math arithmetic)
+puts 'Hens #{25.0+30.0/6.0}'
+puts 'Roosters #{100.0-25.0*3.0%4.0}'
+
+# print a string
+puts 'Now I will count the eggs:'
+
+# print a result of math arithmetic
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# print a string
+puts 'Is it true that 3+2<5-7?'
+
+# print a math arithmetic
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# prints a combination of strings and code (used math arithmetic)
+puts 'what is 3+2? #{3.0+2.0}'
+puts 'What is 5-7? #{5.0-7.0}'
+
+# print a string
+puts 'Oh, that\'s why it\'s false.'
+
+# print a string
+puts 'How about some more.'
+
+# prints a combination of strings and code (logic operator)
+puts 'Is it greater? #{5.0>-2.0}'
+puts 'is it greater or equal? #{5.0>=-2.0}'
+puts 'Is it less or equal? #{5.0<=-2.0}'
