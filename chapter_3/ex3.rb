33a2bd48d0416fba877c21a69a0985c2b875419b
Serg09 <sergeyskumatov@aol.com>
2015-10-04 09:50:35 -0600

    Fixed string interpolation, which requires double quotes and fixed surrounding space
    [#104389658]

--- chapter_3/ex3.rb
+++ chapter_3/ex3.rb
@@ -2,8 +2,8 @@
 puts 'I will now count my chickens:'
 
 # prints a combination of strings and code (used math arithmetic)
-puts 'Hens #{25+30/6}'
-puts 'Roosters #{100-25*3%4}'
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
 
 # print a string
 puts 'Now I will count the eggs:'
@@ -18,8 +18,8 @@ puts 'Is it true that 3+2<5-7?'
 puts 3 + 2 < 5 - 7
 
 # prints a combination of strings and code (used math arithmetic)
-puts 'what is 3+2? #{3+2}'
-puts 'What is 5-7? #{5-7}'
+puts "what is 3+2? #{3 + 2}"
+puts "What is 5-7? #{5 - 7}"
 
 # print a string
 puts 'Oh, that\'s why it\'s false.'
@@ -28,6 +28,6 @@ puts 'Oh, that\'s why it\'s false.'
 puts 'How about some more.'
 
 # prints a combination of strings and code (logic operator)
-puts 'Is it greater? #{5>-2}'
-puts 'is it greater or equal? #{5>=-2}'
-puts 'Is it less or equal? #{5<=-2}'
+puts "Is it greater? #{5 > -2}"
+puts "is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
--- chapter_3/ex3_floating.rb
+++ chapter_3/ex3_floating.rb
@@ -2,8 +2,8 @@
 puts 'I will now count my chickens:'
 
 # prints a combination of strings and code (used math arithmetic)
-puts 'Hens #{25.0+30.0/6.0}'
-puts 'Roosters #{100.0-25.0*3.0%4.0}'
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 # print a string
 puts 'Now I will count the eggs:'
@@ -18,8 +18,8 @@ puts 'Is it true that 3+2<5-7?'
 puts 3.0 + 2.0 < 5.0 - 7.0
 
 # prints a combination of strings and code (used math arithmetic)
-puts 'what is 3+2? #{3.0+2.0}'
-puts 'What is 5-7? #{5.0-7.0}'
+puts "what is 3+2? #{3.0 + 2.0}"
+puts "What is 5-7? #{5.0 - 7.0}"
 
 # print a string
 puts 'Oh, that\'s why it\'s false.'
@@ -28,6 +28,6 @@ puts 'Oh, that\'s why it\'s false.'
 puts 'How about some more.'
 
 # prints a combination of strings and code (logic operator)
-puts 'Is it greater? #{5.0>-2.0}'
-puts 'is it greater or equal? #{5.0>=-2.0}'
-puts 'Is it less or equal? #{5.0<=-2.0}'
+puts "Is it greater? #{5.0 > -2.0}"
+puts "is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
