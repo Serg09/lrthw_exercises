a467bcb23046ab9018515a3a74e33e45733a5575
Serg09 <sergeyskumatov@aol.com>
2015-10-16 22:35:14 -0600

    Add changes to ex10.rb file
    [#104389680]

--- chapter_10/ex10.rb
+++ chapter_10/ex10.rb
@@ -3,9 +3,7 @@ persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 more_tests = "\tHello World!\n\t\t ASCII Hello again!"
 
-fat_cat = """
-I'll do a list:
-\t* Cat food
+fat_cat = """ I'll do a list \"list\": \n\t* Cat food
 \t\f* Fishies
 \t* Catnip\n\t* \\Grass\\\n
 """
